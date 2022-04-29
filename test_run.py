31f9932b6c25f36fe8edd39aa518594ae4f0b035
Jeffrey C.Y. Yu <83930692+jeffreycyyu@users.noreply.github.com>
2022-04-29 19:38:59 -0400

    fixed model name error

--- test_run.py
+++ test_run.py
@@ -13,13 +13,13 @@ from ipynb.fs.full.agent import RESEARCHER
 from ipynb.fs.full.model import SCIENTIFIC_WORLD
 
 
-test_model = SCIENTIFIC_WORLD(10, 20, 20, 'normal', 'normal', 'normal', 'normal', 'normal')
+test_model = SCIENTIFIC_WORLD(30, 20, 20, 'normal', 'normal', 'normal', 'normal', 'normal')
 for i in range(20):
     test_model.step()
 
     
-average_count = model.datacollector.get_model_vars_dataframe()
+average_count = test_model.datacollector.get_model_vars_dataframe()
 average_count.plot()
     
-agent_publication_impact_count = model.datacollector.get_agent_vars_dataframe()
+agent_publication_impact_count = test_model.datacollector.get_agent_vars_dataframe()
 print(agent_publication_impact_count)
