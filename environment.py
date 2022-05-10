40f98346d2ebec7821bcfabdc8e843f2c283f271
Jeffrey C.Y. Yu <83930692+jeffreycyyu@users.noreply.github.com>
2022-05-09 21:20:34 -0400

    Update environment.py

--- environment.py
+++ environment.py
@@ -5,35 +5,58 @@ import pandas as pd
 import random
 
 
-def SIMULATE_SAMPLE_FROM_WORLD(real_world_values, bin_minimum, bin_maximum, bin_size):
+class SIMULATE_SAMPLE_FROM_WORLD:
     """
-    Argument(s):
-        real_world_values: list of values from the real world to model the simulated sampling distribution from
-        bin_minimum: minimum value of variable to simulate a sample of
-        bin_maximum: maximum value of variable to simulate a sample of
-        bin_size: bin size of variable to simulate a sample of (values within a bin will be pooled together to generate sampling probabilities)
+    Create a distribution to sample values from given a list of real-world values
     """
+    def __init__(
+        self,
+        real_world_values: list,
+        bin_minimum: int,
+        bin_maximum: int,
+        bin_size: int,
+        name: str = 'simulate_sample_from_real_world'
+        ):
+        """
+        Argument(s):
+            real_world_values: list of values from the real world to model the simulated sampling distribution from
+            bin_minimum: minimum value of variable to simulate a sample of
+            bin_maximum: maximum value of variable to simulate a sample of
+            bin_size: bin size of variable to simulate a sample of (values within a bin will be pooled together to generate sampling probabilities)
+            name: name
+        """
+        print('class SIMULATE_SAMPLE_FROM_WORLD:.__init__')
 
-    #bin limits, we want to bin into intervals of 5
-    bin_intervals = np.arange(bin_minimum, bin_maximum + bin_size, bin_size).tolist()
+        self.real_world_values = real_world_values
+        self.bin_minimum = bin_minimum
+        self.bin_maximum = bin_maximum
+        self.bin_size = bin_size
+        self._name = name
 
-    #counts for how many professors fall into each bins
-    bin_counts = pd.cut(h_indexes, bins=bin_intervals).value_counts()
+    
+    def __call__(self):
+    
+        #bin limits, we want to bin into intervals of 5
+        bin_intervals = np.arange(bin_minimum, bin_maximum + bin_size, bin_size).tolist()
 
-    #probabilities of a professor belonging to a given bin
-    bin_probabilities = [float(i)/sum(bin_counts) for i in bin_counts]
+        #counts for how many professors fall into each bins
+        bin_counts = pd.cut(h_indexes, bins=bin_intervals).value_counts()
 
-    #sample to see which bin the sample will fall into with probabilities given
-    bin_choice = random.choices(range(1, len(bin_intervals)), bin_probabilities)
+        #probabilities of a professor belonging to a given bin
+        bin_probabilities = [float(i)/sum(bin_counts) for i in bin_counts]
 
-    #uniformly sample an h-index within the given bin choice
-    simulated_sample_h_index = random.uniform(bin_intervals[bin_choice[0]-1], bin_intervals[bin_choice[0]])
+        #sample to see which bin the sample will fall into with probabilities given
+        bin_choice = random.choices(range(1, len(bin_intervals)), bin_probabilities)
 
-    #simulated h_index given real values of all psychology professors at UBC
-    return simulated_sample_h_index
-  
-  
-  
+        #uniformly sample an h-index within the given bin choice
+        simulated_sample_h_index = random.uniform(bin_intervals[bin_choice[0]-1], bin_intervals[bin_choice[0]])
+
+        #simulated h_index given real values of all psychology professors at UBC
+        return simulated_sample_h_index
+    
+    
+    
+    
 #H-INDEX (TEST)
 
 #list of UBC psychology professor h-indexes; values taken from google scholar
@@ -43,9 +66,9 @@ bin_minimum = 0
 bin_maximum = 100
 bin_size = 5
 
-simulated_ubc_psychology_h_index = int(SIMULATE_SAMPLE_FROM_WORLD(h_indexes_ubc_psychology, bin_minimum, bin_maximum, bin_size))
+simulated_ubc_psychology_h_index = SIMULATE_SAMPLE_FROM_WORLD(h_indexes_ubc_psychology, bin_minimum, bin_maximum, bin_size)
 
-print(simulated_ubc_psychology_h_index)
+print(simulated_ubc_psychology_h_index())
   
   
  
