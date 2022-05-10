692e8450bc03683c186d0503773bedbeced3cfae
Jeffrey C.Y. Yu <83930692+jeffreycyyu@users.noreply.github.com>
2022-05-09 20:27:23 -0400

    bin size

--- h_index_simulation.py
+++ h_index_simulation.py
@@ -5,11 +5,14 @@ import pandas as pd
 import random
 
 #list of UBC psychology professor h-indexes; values taken from google scholar
-#CHANGE_ME (unfinished) need to add all values
+#CHANGE_ME (unfinished)
 h_indexes = [61, 27, 41, 27, 25, 45, 95, 17, 49, 55, 15, 71, 72]
 
+#how big we want each bin to be
+bin_size = 5
+
 #bin limits, we want to bin into intervals of 5
-bin_intervals = np.arange(0, 105, 5).tolist()
+bin_intervals = np.arange(0, 100 + bin_size, bin_size).tolist()
 
 #counts for how many professors fall into each bins
 bin_counts = pd.cut(h_indexes, bins=bin_intervals).value_counts()
@@ -18,7 +21,7 @@ bin_counts = pd.cut(h_indexes, bins=bin_intervals).value_counts()
 bin_probabilities = [float(i)/sum(bin_counts) for i in bin_counts]
 
 #sample to see which bin the sample will fall into with probabilities given
-bin_choice = random.choices(range(1, 21), bin_probabilities)
+bin_choice = random.choices(range(1, len(bin_intervals)), bin_probabilities)
 
 #uniformly sample an h-index within the given bin choice
 simulated_sample_h_index = random.uniform(bin_intervals[bin_choice[0]-1], bin_intervals[bin_choice[0]])
